ed7ddec242468f884a93fee9dc596b3d6b7f4608
Alexorcizet <alexorcizet87@gmail.com>
2023-03-12 21:10:44 +0200

    public folder added

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const cors = require('cors')
+const formService = require('./service/form.service.js')
+const path = require('path')
+
+const app = express()
+const http = require('http').createServer(app)
+
+app.use(express.json())
+app.use(express.static('public'))
+
+if (process.env.NODE_ENV === 'production') {
+    // Express serve static files on production environment
+    app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+    // Configuring CORS
+    const corsOptions = {
+        // Make sure origin contains the url your frontend is running on
+        origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+        credentials: true,
+    }
+    app.use(cors(corsOptions))
+}
+
+
+
+app.post('/api/stationName', async (req, res) => {
+    const isMailSent = await formService.sendData(req.body)
+    res.send(isMailSent)
+})
+
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+const port = process.env.PORT || 3030
+
+http.listen(port, () => {
+    console.log('Server is running on port: ' + port)
+})
+
+
+
---
+++ service/form.service.js
@@ -0,0 +1,41 @@
+
+const nodemailer = require('nodemailer')
+
+async function sendData(facilityData) {
+    const stations = facilityData.stations
+    let csv = ''
+    stations.forEach(station => csv += station.join() + '\n')
+    return new Promise((resolved, reject) => {
+
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'locationsform@gmail.com',
+                pass: 'dmoqmghwonxhpajo'
+            }
+        })
+
+        let mailOptions = {
+            from: 'locationsform@gmail.com',
+            to: 'alexorcizet87@gmail.com',
+            subject: `טופס מיקומים ל${facilityData.facility}`,
+            attachments: [{
+                filename: `${facilityData.facility}.csv`,
+                content: csv
+            }]
+        }
+
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                console.log('err:', err)
+                resolved(false)
+            } else {
+                resolved(true)
+            }
+        })
+    })
+}
+
+module.exports = {
+    sendData,
+}
