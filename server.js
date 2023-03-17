4b9bfd6a1526719a8d78457fbe86a3e1931cbbf2
Alexorcizet <alexorcizet87@gmail.com>
2023-03-17 21:26:10 +0200

    responsive adjustment

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const formService = require('./service/form.service.js')
 const path = require('path')
 
 const app = express()
-const http = require('http').createServer(app)
-
 app.use(express.json())
 app.use(express.static('public'))
 
